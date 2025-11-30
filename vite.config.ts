832c566c710b7f0e7e1a63ae441a651c6dc53a56
baohuy.dev <baohuy.le@brayleinosplash.com>
2025-11-30 17:54:01 +0700

    update: vite allow dns

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['shrcspin.xyz'],
+    host: true,
+    port: 4173,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
